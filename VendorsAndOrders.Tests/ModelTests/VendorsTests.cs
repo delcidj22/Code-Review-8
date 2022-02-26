1bc85bc22498c72bc8f7d51d600dbf046e995086
delcidj22 <jdelcid23@gmail.com>
2022-02-25 22:02:28 -0800

    Pass Test 5

--- VendorsAndOrders.Tests/ModelTests/VendorsTests.cs
+++ VendorsAndOrders.Tests/ModelTests/VendorsTests.cs
@@ -61,6 +61,30 @@ namespace VendorsAndOrders.Tests
       Vendor newVendor1 = new Vendor(name01);
       Vendor newVendor2 = new Vendor(name02);
       List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+      //Act 
+      List<Vendor> result = Vendor.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      //Arrange 
+      string name01 = "Pierre's Bakery";
+      string name02 = "Jon's Bakery";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+
+      //Act 
+      Vendor result = Vendor.Find(2);
+
+      //Assert
+      Assert.AreEqual(newVendor2, result);
+    }
+
+
   }
 }
