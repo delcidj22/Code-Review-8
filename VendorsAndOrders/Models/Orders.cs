7248ebfe9502ab51b46bd467249bb662f5f976d7
delcidj22 <jdelcid23@gmail.com>
2022-02-25 22:18:09 -0800

    Update vendors and orders Model

--- VendorsAndOrders/Models/Orders.cs
+++ VendorsAndOrders/Models/Orders.cs
@@ -4,13 +4,19 @@ namespace VendorsAndOrders.Models
 {
   public class Order
   {
+    public string Name { get; set; }
     public string Description { get; set; }
+    public string Price { get; set; }
+    public int Id { get; }
     private static List<Order> _instances = new List<Order> {};
 
-    public Order(string description)
+    public Order(string description, string name, string price)
     {
+      Name = name;
       Description = description;
+      Price = price;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public static List<Order> GetAll()
@@ -21,6 +27,11 @@ namespace VendorsAndOrders.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
